5202a8311405992b9f543dd750e54bfd1d2b71e2
Christian Esperon <107196334+Chrissy-ness@users.noreply.github.com>
2022-09-18 16:42:57 -0400

    Update timers-practice.js

--- timers-practice.js
+++ timers-practice.js
@@ -1,17 +1,17 @@
 //COUNTDOWN EXERCISE
 
-// function countDown(time) {
-//     let timer = setInterval(function() {
-//         time--; 
-//         if(time <= 0) {
-//             clearInterval(timer);
-//             console.log("DONE!");
-//         }
-//         else {
-//             console.log(time);
-//         }
-//     }, 1000)
-// }
+function countDown(time) {
+    let timer = setInterval(function() {
+        time--; 
+        if(time <= 0) {
+            clearInterval(timer);
+            console.log("DONE!");
+        }
+        else {
+            console.log(time);
+        }
+    }, 1000)
+}
 
 //RANDOM GAME
 
